644a8a254ddc5ea32d15679413f7f20e8b05c5d3
zheng-008 <oceandev999@gmail.com>
2021-09-16 19:56:21 +0900

    staking contract so far and testing

---
+++ staking & test/staking.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/**
+* @notice Stakeable is a contract who is ment to be inherited by other contract that wants Staking capabilities
+*/
+contract Stakeable {
+
+
+    /**
+    * @notice Constructor,since this contract is not ment to be used without inheritance
+    * push once to stakeholders for it to work proplerly
+     */
+    constructor() {
+    }
+    /**
+     * @notice
+     * A stake struct is used to represent the way we store stakes, 
+     * A Stake will contain the users address, the amount staked and a timestamp, 
+     * Since which is when the stake was made
+     */
+    struct Stake{
+        address user;
+        uint256 amount;
+        uint256 since;
+    }
+    /**
+    * @notice Stakeholder is a staker that has active stakes
+     */
+    struct Stakeholder{
+        address user;
+        Stake[] address_stakes;
+        
+    }
+
+    /**
+    * @notice 
+    *   This is a array where we store all Stakes that are performed on the Contract
+    *   The stakes for each address are stored at a certain index, the index can be found using the stakes mapping
+    */
+    Stakeholder[] internal stakeholders;
+    /**
+    * @notice 
+    * stakes is used to keep track of the INDEX for the stakers in the stakes array
+     */
+    mapping(address => uint256) internal stakes;
+    /**
+    * @notice Staked event is triggered whenever a user stakes tokens, address is indexed to make it filterable
+     */
+     event Staked(address indexed user, uint256 amount, uint256 index, uint256 timestamp);
+
+     /**
+    * @notice _addStakeholder takes care of adding a stakeholder to the stakeholders array
+     */
+    function _addStakeholder(address staker) internal returns (uint256){
+        // Push a empty item to the Array to make space for our new stakeholder
+        stakeholders.push();
+        // Calculate the index of the last item in the array by Len-1
+        uint256 userIndex = stakeholders.length - 1;
+        // Assign the address to the new index
+        stakeholders[userIndex].user = staker;
+        // Add index to the stakeHolders
+        stakes[staker] = userIndex;
+        return userIndex; 
+    }
+
+    /**
+    * @notice
+    * _Stake is used to make a stake for an sender. It will remove the amount staked from the stakers account and place those tokens inside a stake container
+    * StakeID 
+    */
+    function _stake(uint256 _amount) internal{
+        // Simple check so that user does not stake 0 
+        require(_amount > 0, "Cannot stake nothing");
+        
+
+        // Mappings in solidity creates all values, but empty, so we can just check the address
+        uint256 index = stakes[msg.sender];
+        // block.timestamp = timestamp of the current block in seconds since the epoch
+        uint256 timestamp = block.timestamp;
+        // See if the staker already has a staked index or if its the first time
+        if(index == 0){
+            // This stakeholder stakes for the first time
+            // We need to add him to the stakeHolders and also map it into the Index of the stakes
+            // The index returned will be the index of the stakeholder in the stakeholders array
+            index = _addStakeholder(msg.sender);
+        }
+
+        // Use the index to push a new Stake
+        // push a newly created Stake with the current block timestamp.
+        stakeholders[index].address_stakes.push(Stake(msg.sender, _amount, timestamp));
+        // Emit an event that the stake has occured
+        emit Staked(msg.sender, _amount, index,timestamp);
+    }
+
+    /**
+    * Add functionality like burn to the _stake afunction
+    *
+     */
+    function stake(uint256 _amount) public {
+      // Make sure staker actually is good for it
+      require(_amount < _balances[msg.sender], "DevToken: Cannot stake more than you own");
+
+        _stake(_amount);
+                // Burn the amount of tokens on the sender
+        _burn(msg.sender, _amount);
+    }
+
+}
